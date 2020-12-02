8126430fa14b28daf7ad6300ed943e508a4e1a77
Zenahr <zenmatica@gmail.com>
2020-12-03 00:39:15 +0100

    improve Cookie Backer

--- src/Cookie_Backer.py
+++ src/Cookie_Backer.py
@@ -18,9 +18,13 @@ def init():
         print('Cookie Backer: Loading complete.')
         print('Please log into your account.')
         input("when you're logged in, press ENTER here to finish backing the cookies.")
+<<<<<<< HEAD
         if not os.path.exists(directory):
                 os.makedirs(directory)
         pickle.dump(DRIVER.get_cookies() , open(directory+"cookies.pkl","wb"))
+=======
+        pickle.dump(DRIVER.get_cookies() , open("./cookies/cookies.pkl","wb"))
+>>>>>>> c77bb62a0e0d81f83239015b02b09895313c90d1
         print('Cookies are out of the oven and ready to be fed to the JSON generator!')
         print('Please head over to the installation folder and run "Generate_JSON.exe"')
         input('You can close this window now.')
