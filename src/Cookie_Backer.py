c884f3c03629cb45463061d443510fb20506c5a8
Zenahr <zenmatica@gmail.com>
2020-12-03 00:36:43 +0100

    improve Cookie Backer

--- src/Cookie_Backer.py
+++ src/Cookie_Backer.py
@@ -5,8 +5,10 @@ This helps users get started with their personal cookie pickle file generation.
 import pickle
 from time import sleep
 import selenium.webdriver as webdriver
+import os
 
 def init():
+        directory = './test/cookies/'
         options = webdriver.ChromeOptions()
         options.add_argument('--ignore-certificate-errors')
         COOKIE_URL = 'https://www.unrealengine.com/en-US/'
@@ -16,7 +18,11 @@ def init():
         print('Cookie Backer: Loading complete.')
         print('Please log into your account.')
         input("when you're logged in, press ENTER here to finish backing the cookies.")
-        pickle.dump(DRIVER.get_cookies() , open("./cookies/cookies.pkl","wb"))
+        if not os.path.exists(directory):
+                os.makedirs(directory)
+        pickle.dump(DRIVER.get_cookies() , open(directory+"cookies.pkl","wb"))
         print('Cookies are out of the oven and ready to be fed to the JSON generator!')
         print('Please head over to the installation folder and run "Generate_JSON.exe"')
         input('You can close this window now.')
+
+init()
