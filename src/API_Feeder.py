aa8134e13af31cafaaf7713c47ac88db39b4dada
Zenahr <zenmatica@gmail.com>
2020-12-02 22:01:09 +0100

    add logging and fix timeout error

--- src/API_Feeder.py
+++ src/API_Feeder.py
@@ -1,4 +1,5 @@
 import pickle
+from time import sleep
 import selenium.webdriver as webdriver
 import time
 from bs4 import BeautifulSoup
@@ -8,28 +9,30 @@ import json
 
 class API_Feeder:
     def __init__(self):
-        self.__current_feed = None # Current HTML source for bs4
-        options    = webdriver.ChromeOptions()
+        self.retriever = Retriever()
+        options = webdriver.ChromeOptions()
         options.add_argument('--ignore-certificate-errors')
         # options.add_argument('--headless') # TODO: Add in API prod code
         self.COOKIE_URL = 'https://www.unrealengine.com/en-US/'
         self.VAULT_URL  = 'https://www.unrealengine.com/marketplace/en-US/vault'
         self.DRIVER     = webdriver.Chrome(chrome_options=options)
         self.DRIVER.maximize_window()
+        self.DRIVER.get(self.COOKIE_URL)
         self.cookies = pickle.load(open("cookies.pkl", "rb"))
         for cookie in self.cookies:
             self.DRIVER.add_cookie(cookie)
+        self.DRIVER.get(self.VAULT_URL)
+        time.sleep(3)
     
     def get_current_feed(self):
         return self.DRIVER.page_source
 
     def next_page(self):
-        time.sleep(2)
+        time.sleep(3)
         pagination_button = self.DRIVER.find_element_by_class_name('rc-pagination-next').find_element_by_tag_name('a') # li element. <a> is nested as immediate child.
         active_page_number = self.DRIVER.find_element_by_class_name('rc-pagination-item-active').find_element_by_tag_name('a').get_attribute('innerHTML')
         print('API_Feeder: active page:', active_page_number)
         pagination_button.click()
-        time.sleep(1)
 
     def generate_API_JSON_feed(self):
         """
@@ -39,10 +42,11 @@ class API_Feeder:
         returns:
             JSON
         """
-        retriever = Retriever()
+        
         data = []
-        for i in range(0, 20):
-            data.append(retriever.retrieve(self.get_current_feed))
+        for i in range(0, 3):
+            sleep(2)
+            data.append(self.retriever.retrieve(self.get_current_feed()))
             self.next_page()
             print('API Feeder: generated JSON from webpage')
         print('API Feeder: Finished generating JSON objects')
@@ -57,3 +61,7 @@ class API_Feeder:
     def __save_cookies(self):
         self.DRIVER.get(self.COOKIE_URL)
         pickle.dump( self.DRIVER.get_cookies() , open("cookies.pkl","wb"))
+
+runtime = API_Feeder()
+json = runtime.generate_API_JSON_feed()
+print(json)
--- src/VaultItemInfoRetriever.py
+++ src/VaultItemInfoRetriever.py
@@ -16,22 +16,29 @@ class Retriever:
         Returns:
             [{}]: List of all Vault Items found on the page
         """
-        soup = BeautifulSoup(source)
+        soup = BeautifulSoup(source, features='html.parser')
         vault_items = []
         container = soup.find('div', class_='category-container vault-page')
-        items = soup.find_all('div', class_='asset-container')
-        for item in items:
-            try:
-                item_data = {
-                'name' : item.find('a', class_='mock-ellipsis-item mock-ellipsis-item-helper ellipsis-text').get_text(),
-                'link' : 'https://www.unrealengine.com' + item.find('a', class_='mock-ellipsis-item mock-ellipsis-item-helper ellipsis-text')['href'],
-                'publisher' : item.find('div', class_='creator ellipsis').get_text(),
-                'amount_of_ratings' : item.find('span', class_='rating-board__count').get_text(),
-                'img_src' : item.find('img')['src'],
-                }
-                vault_items.append(item_data)
-            except:
-                pass
-        return vault_items
+        if (not container):
+            print('Retriever: container div element not detected')
+            return 1
+        items = container.find_all('div', class_='asset-container')
+        if(items):
+            for item in items:
+                try:
+                    item_data = {
+                    'name' : item.find('a', class_='mock-ellipsis-item mock-ellipsis-item-helper ellipsis-text').get_text(),
+                    'link' : 'https://www.unrealengine.com' + item.find('a', class_='mock-ellipsis-item mock-ellipsis-item-helper ellipsis-text')['href'],
+                    'publisher' : item.find('div', class_='creator ellipsis').get_text(),
+                    'amount_of_ratings' : item.find('span', class_='rating-board__count').get_text(),
+                    'img_src' : item.find('img')['src'],
+                    }
+                    vault_items.append(item_data)
+                except:
+                    pass
+            return vault_items
+        else:
+            print('Retriever: No Vault Items Detected.')
+
 
 Retriever()
